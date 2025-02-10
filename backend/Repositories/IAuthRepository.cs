df12b5b50e3ac5eaea76cdb06cb69e0ca6a49638
TongQuangHung102 <hungcma2002@gmail.com>
2025-02-10 15:54:56 +0700

    Loại bỏ thư mục .vs/ khỏi tracking

--- backend/Repositories/IAuthRepository.cs
+++ backend/Repositories/IAuthRepository.cs
@@ -5,6 +5,8 @@ namespace backend.Repositories
     public interface IAuthRepository
     {
         Task AddUser(User user);
+        Task UpdateUser(User user);
+        Task<User> GetUserByToken(string token);
         Task<User> GetUserByEmail(string email);
         Task UpdatePasswordAsync(string email, string newPassword);
         Task UpdateLastLoginTimeAsync(User user);
--- backend/Services/RegisterService.cs
+++ backend/Services/RegisterService.cs
@@ -9,10 +9,12 @@ namespace backend.Services
     public class RegisterService
     {
         private readonly IAuthRepository _authRepository;
-
-        public RegisterService(IAuthRepository authRepository)
+        private readonly SendMailService _sendMailService;
+        private readonly string _frontendUrl = "http://localhost:3000/";
+        public RegisterService(IAuthRepository authRepository, SendMailService sendMailService)
         {
             _authRepository = authRepository;
+            _sendMailService = sendMailService;
         }
 
         public async Task<IActionResult> Register(RegisterDto model)
@@ -31,14 +33,37 @@ namespace backend.Services
                 Password = hashedPassword,
                 FullName = model.FullName,
                 Role = model.Role,
-                IsActive = model.IsActive,
-                CreateAt = DateTime.UtcNow
+                IsActive = false,
+                CreateAt = DateTime.UtcNow,
+                EmailVerificationToken = Guid.NewGuid().ToString(),
+                TokenExpiry = DateTime.UtcNow.AddHours(24)
             };
 
 
             await _authRepository.AddUser(user);
-
-            return new OkObjectResult(new { message = "User registered successfully!" });
+            await _sendMailService.SendConfirmationEmailAsync(user.Email, user.EmailVerificationToken);
+            return new OkObjectResult(new { message = "User registered successfully! Please check your email to confirm your account." });
         }
+        public async Task<IActionResult> ConfirmEmailAsync(string token)
+        {
+            var user = await _authRepository.GetUserByToken(token);
+            if (user == null)
+            {
+                return new RedirectResult($"{_frontendUrl}?status=invalid_token");
+            }
+
+            if (user.TokenExpiry < DateTime.UtcNow)
+            {
+                return new RedirectResult($"{_frontendUrl}?status=token_expired");
+            }
+
+            user.IsActive = true;
+            user.EmailVerificationToken = null;
+            user.TokenExpiry = null;
+            await _authRepository.UpdateUser(user);
+
+            return new RedirectResult($"{_frontendUrl}?status=success");
+        }
+
     }
 }
