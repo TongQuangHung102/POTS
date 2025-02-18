44b2ee0b390fc058cd37d1dd07bee21612aca13a
TongQuangHung102 <hungcma2002@gmail.com>
2025-02-18 20:02:08 +0700

    Add FE Package

--- frontend/src/rout/AppRoutes.js
+++ frontend/src/rout/AppRoutes.js
@@ -5,8 +5,9 @@ import Register from '../pages/RegisterForm';
 import Login from '../pages/LoginForm';
 import ListChapter from '../pages/admin/ListChapter';
 import { useAuth } from '../hooks/useAuth';
-
-
+import ListPackage from '../pages/admin/ListPackage';
+import PackageDetail from '../pages/admin/PackageDetail';
+import AddPackage from '../pages/admin/AddPackage';
 const AppRoutes = () => {
     const { user, loading } = useAuth(); // Lấy cả user và loading
 
@@ -18,18 +19,22 @@ const AppRoutes = () => {
     return (
         <Routes>
             {/* Route cho login */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/admin" element={<AdminLayout />}>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/admin' element={<AdminLayout />}>
                 <Route index element={<AdminDashboard />} />
-                <Route path="/admin/listchapter" element={<ListChapter />} />
+                <Route path='/admin/listchapter' element={<ListChapter />} />
+                <Route path='/admin/listpackage' element={<ListPackage />} />
+                <Route
+                    path='/admin/package/:planId'
+                    element={<PackageDetail />}
+                />
+                <Route path='/admin/addpackage/' element={<AddPackage />} />
             </Route>
             {/* Nếu chưa đăng nhập, điều hướng đến login */}
-            {!user && <Route path="/" element={<Navigate to="/login" replace />} />}
-
-
-
-
+            {!user && (
+                <Route path='/' element={<Navigate to='/login' replace />} />
+            )}
         </Routes>
     );
 };
