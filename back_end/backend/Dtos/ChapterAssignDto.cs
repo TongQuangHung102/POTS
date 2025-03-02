3a0797832f9eab6806c5b89d4b5d816da7452210
TongQuangHung102 <hungcma2002@gmail.com>
2025-03-02 21:10:42 +0700

    Update BE FE AssignChapter

---
+++ back_end/backend/Dtos/ChapterAssignDto.cs
@@ -0,0 +1,14 @@
+﻿namespace backend.Dtos
+{
+    public class ChapterAssignDto
+    {
+        public int ChapterId { get; set; }
+        public string ChapterName { get; set; }
+        public bool IsVisible { get; set; }
+        public int Order { get; set; }
+        public int Semester { get; set; }
+        public int? UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? GradeName { get; set; }
+    }
+}
